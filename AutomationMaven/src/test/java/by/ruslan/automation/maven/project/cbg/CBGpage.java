86bb71ad36be6169cb3b1aeb011dc92bc86410b9
Ruslan <r.vasilyeu@gmail.com>
2018-09-21 16:25:34 -0400

    wait must happened before driver switches the page

--- AutomationMaven/src/test/java/by/ruslan/automation/maven/project/cbg/CBGpage.java
+++ AutomationMaven/src/test/java/by/ruslan/automation/maven/project/cbg/CBGpage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import by.ruslan.automation.maven.entity.PageObject;
 
@@ -32,14 +33,16 @@ public class CBGpage extends PageObject{
 	
 	public void openHomeURL() {
 		super.openHomePage(homeURL);
-		//Alert alert = driver.switchTo().alert();
+		
 	}
 	
 	
 	public void search(String str) {
+		
 		searchID.sendKeys(str);		
+		wait.until(ExpectedConditions.visibilityOf(searchID));
 		submitButton.click();
-		//wait.until(ExpectedConditions.visibilityOf(searchID));
+		
 	}
 	
 	public int getRows() {

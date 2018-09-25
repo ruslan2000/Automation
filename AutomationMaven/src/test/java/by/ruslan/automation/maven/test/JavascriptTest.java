11bd2bc04311f934e71e0c3a214ca44bd828c451
Ruslan <r.vasilyeu@gmail.com>
2018-09-25 11:13:17 -0400

    Javascript Executer

---
+++ AutomationMaven/src/test/java/by/ruslan/automation/maven/test/JavascriptTest.java
@@ -0,0 +1,46 @@
+package by.ruslan.automation.maven.test;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import by.ruslan.automation.maven.entity.PageObject;
+
+public class JavascriptTest extends PrepareWebTest {
+
+	private JavascriptExecutor js;
+	private PageObject page;
+
+	@BeforeMethod
+	public void preSet() {
+		js = (JavascriptExecutor) driver;
+		page = new PageObject(driver);
+	}
+
+	@Test
+	public void f() throws InterruptedException {
+
+		page.openHomePage("http://moneyboats.com/");
+
+		page.sleep(1000);
+
+		// Vertical scroll down by 600 pixels
+		// js.executeScript("window.scrollBy(0,600)");
+		// till the very bottom
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+
+		page.sleep(1000);
+
+		// Vertical scroll up
+		js.executeScript("window.scrollTo(0, 0)");
+
+		page.sleep(1000);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		page.cleanPage();
+	}
+
+}

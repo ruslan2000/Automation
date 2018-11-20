33d1a97d56f62344d26d0bab8116d802ef769908
Ruslan <r.vasilyeu@gmail.com>
2018-11-20 15:34:57 -0500

    up

--- AutomationMaven/src/test/java/by/ruslan/automation/maven/test/TestImages.java
+++ AutomationMaven/src/test/java/by/ruslan/automation/maven/test/TestImages.java
@@ -6,30 +6,35 @@ import org.testng.annotations.Test;
 
 import by.ruslan.automation.maven.entity.PageObject;
 
-public class TestImages extends PrepareWebTest{
-	
-	
+public class TestImages extends PrepareWebTest {
+
 	private PageObject page;
 	private JavascriptExecutor js;
-	
+
 	@BeforeTest
 	public void init() {
 		page = new PageObject(driver);
 		js = (JavascriptExecutor) driver;
 	}
-	
+
 	@Test
 	public void test() throws InterruptedException {
-		String URL = "https://dev-us.pandora.net/en/gifts/gifts-for-daughters/";
+		//String URL = "https://dev-us.pandora.net/en/gifts/gifts-for-daughters/";
+		String URL = "https://dev-us.pandora.net/en/necklaces/view-all";
 		page.openHomePage(URL);
-		
-		page.sleep(2000);
-		
-		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
-		
-		page.sleep(5000);
+
+		int i = 0;
+		while( i < 10) {
+			loadMoreImages(js, page);
+			i++;
+		}
 		
 		page.showNoImages();
+	}
+
+	private void loadMoreImages(JavascriptExecutor js, PageObject page) throws InterruptedException {
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+		page.sleep(2000);
 	}
 
 }

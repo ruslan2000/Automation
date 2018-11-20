b22fb9f42aae957b037f66b7ff4da000d199afca
Ruslan <r.vasilyeu@gmail.com>
2018-11-20 15:25:44 -0500

    up

--- AutomationMaven/src/test/java/by/ruslan/automation/maven/test/TestImages.java
+++ AutomationMaven/src/test/java/by/ruslan/automation/maven/test/TestImages.java
@@ -1,5 +1,6 @@
 package by.ruslan.automation.maven.test;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -9,16 +10,24 @@ public class TestImages extends PrepareWebTest{
 	
 	
 	private PageObject page;
+	private JavascriptExecutor js;
 	
 	@BeforeTest
 	public void init() {
 		page = new PageObject(driver);
+		js = (JavascriptExecutor) driver;
 	}
 	
 	@Test
-	public void test() {
+	public void test() throws InterruptedException {
 		String URL = "https://dev-us.pandora.net/en/gifts/gifts-for-daughters/";
 		page.openHomePage(URL);
+		
+		page.sleep(2000);
+		
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+		
+		page.sleep(5000);
 		
 		page.showNoImages();
 	}
